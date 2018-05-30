0a7c370ee168c689b7d37fe7182e8f017924f8f9
ALostTeddyBear <AtlasFlame_@hotmail.com>
2018-05-30 12:47:27 +0200

    Fixed stuff

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => ['auth']], function()
+{
+    Route::resource('roles', 'RoleController');
+    Route::resource('users', 'UserController');
+    Route::resource('products', 'ProductController');
+});
