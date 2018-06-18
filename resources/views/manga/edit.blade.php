6b0ee0115f23fae7eb8f0d7d095599b9e3df930f
ALostTeddyBear <AtlasFlame_@hotmail.com>
2018-06-18 21:46:42 +0200

    Edit stuff

--- resources/views/manga/edit.blade.php
+++ resources/views/manga/edit.blade.php
@@ -34,31 +34,31 @@
              <div class="col-xs-12 col-sm-12 col-md-12">
  		        <div class="form-group">
  		            <strong>Title:</strong>
- 		            <input type="text" name="title" id='title' class="form-control" placeholder="Title">
+ 		            <input type="text" name="title" id='title' value="{{$manga->title}}" class="form-control" placeholder="Title">
  		        </div>
  		    </div>
              <div class="col-xs-12 col-sm-12 col-md-12">
  		        <div class="form-group">
  		            <strong>Chapters:</strong>
- 		            <input type="text" name="chapters" id='chapters' class="form-control" placeholder="Chapters">
+ 		            <input type="text" name="chapters" id='chapters' value="{{$manga->chapters}}" class="form-control" placeholder="Chapters">
  		        </div>
  		    </div>
              <div class="col-xs-12 col-sm-12 col-md-12">
  		        <div class="form-group">
  		            <strong>Volumes:</strong>
- 		            <input type="text" name="volumes" id='volumes' class="form-control" placeholder="Volumes">
+ 		            <input type="text" name="volumes" id='volumes' value="{{$manga->volumes}}" class="form-control" placeholder="Volumes">
  		        </div>
  		    </div>
  		    <div class="col-xs-12 col-sm-12 col-md-12">
  		        <div class="form-group">
  		            <strong>Description:</strong>
- 		            <textarea class="form-control" style="height:150px" name="description" id='description' placeholder="Description"></textarea>
+ 		            <textarea class="form-control" style="height:150px" name="description" value="{{$manga->description}}" id='description' placeholder="Description"></textarea>
  		        </div>
  		    </div>
              <div class="col-xs-12 col-sm-12 col-md-12">
  		        <div class="form-group">
  		            <strong>Published:</strong>
- 		            <input type="date" name="published" id='published' class="form-control" placeholder="Published">
+ 		            <input type="date" name="published" id='published' class="form-control" value="{{$manga->published}}" placeholder="Published">
  		        </div>
  		    </div>
 		    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
